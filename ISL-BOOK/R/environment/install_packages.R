562add7b7ba77122440958711dbadabcec3d1a85
Amauri Silva <amaurisilva@eoczone.com>
2021-09-29 06:19:14 -0300

    basic document informations

---
+++ ISL-BOOK/R/environment/install_packages.R
@@ -0,0 +1,12 @@
+# ::::
+# To organize and install all dependencies (packages) it is needed.
+# ::::
+install.packages("shiny")
+#
+# ::::
+# ::::
+# Shortcut on RStudio https://appsilon.com/rstudio-shortcuts-and-tips/
+# Introduce to RVEN https://rstudio.github.io/renv/
+#                   https://cran.r-project.org/web/packages/renv/vignettes/renv.html
+# ::::
+# ::::
---
+++ ISL-BOOK/R/environment/run_lib.R
@@ -0,0 +1,50 @@
+# ::::
+# ==============================================================================
+# To manipulate R Virtual Environment
+# renv::restore()
+# renv::status()
+# renv::snapshot()
+# ::::
+# ==============================================================================
+# API and libraries notes/definitions
+# ------------------------------------------------------------------------------
+# dplyr:::::::: functions; filter() arrange(), mutate(), rename(), select(), slice(), summarise()
+# ::::
+# ==============================================================================
+# ::::
+# To running all library at start environment
+library(nycflights13)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+# library(hrbrthemes)
+library(styler)
+library(GGally)
+library(gmodels)
+library(rpart.plot)
+library(partykit)
+library(gapminder)
+library(anytime)
+# library(ggmap)
+library(gridExtra)
+library(cowplot)
+library(viridis)
+library(stringr)
+library(lubridate)
+library(gridExtra)
+library(ggrepel)
+library(data.table)
+# Allow us to calculate percentes erros in an easy way
+library(MLmetrics)
+library(rms)
+library(classifierplots)
+library(rpart.plot)
+library(randomForest)
+library(hmeasure)
+library(zoo)
+# Forecasting Series
+library(forecast)
+# To running nycflights13 dataset
+# library(nycflights13)
+
